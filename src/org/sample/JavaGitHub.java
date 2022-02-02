360266214337e663cc63c7d48fe5f91b4d0210d9
Sivapraba <sivasun2709@gmail.com>
2022-02-02 21:14:42 +0530

    siva done

--- src/org/sample/JavaGitHub.java
+++ src/org/sample/JavaGitHub.java
@@ -5,4 +5,7 @@ public class JavaGitHub {
 	private void empId() {
 		System.out.println("Emp id printed");
 	}
+	private void empName() {
+      System.out.println("Emp name printed by siva");
+	}
 }
