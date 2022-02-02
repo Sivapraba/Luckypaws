aa13bec907cc83d9ea0452ac078ba4612102e610
Sivapraba <sivasun2709@gmail.com>
2022-02-02 21:23:26 +0530

    nandy done

--- src/org/sample/JavaGitHub.java
+++ src/org/sample/JavaGitHub.java
@@ -4,4 +4,8 @@ public class JavaGitHub {
 	private void empId() {
 		System.out.println("Emp id printed");
 	}
+
+	private void empAdd() {
+		System.out.println("emp address printed by nandy");
+	}
 }
